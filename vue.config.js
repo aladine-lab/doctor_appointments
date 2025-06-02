c6deca7115999dab1edccc9e2c11eaebb49824cd
AlaDine <allaeddineala@gmail.com>
2025-06-02 01:37:04 +0100

    Initial doctor_appointments upload

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/doctor_appointments/',
+  publicPath: '/doctor_appointments/'
 })
